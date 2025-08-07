943b71539b7c8b410a2056ba6316289d56bd21a3
soumyab92 <soumyabhattacharya.kgp@gmail.com>
2025-08-07 16:06:51 +0530

    Your commit message

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
+// Importing configureStore from Redux Toolkit to create the Redux store
 import { configureStore } from '@reduxjs/toolkit';
+
+// Importing the cart reducer from the cartSlice file
 import cartReducer from './cartSlice';
 
+// Creating the Redux store using Redux Toolkit's configureStore method
 const store = configureStore({
+  // The reducer field is an object where we define slices of state
+  // In this case, we have a 'cart' slice managed by cartReducer
   reducer: {
-    cart: cartReducer,
+    cart: cartReducer, // This means state.cart will be controlled by cartReducer
   },
 });
 
