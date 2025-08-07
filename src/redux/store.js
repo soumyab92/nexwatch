9af2c61d2ae94251df41fdc18137c337abb6b89c
soumyab92 <soumyabhattacharya.kgp@gmail.com>
2025-08-07 16:04:21 +0530

    Your commit message

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,10 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import cartReducer from './cartSlice';
-
-// const store = configureStore({
-//   reducer: {
-//     cart: cartReducer,
-//   },
-// });
-
-// export default store;
-
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 
-// Get cart from localStorage
-const savedCart = JSON.parse(localStorage.getItem('cart')) || { items: [] };
-
-// Create the store with preloaded state
 const store = configureStore({
   reducer: {
     cart: cartReducer,
   },
-  preloadedState: {
-    cart: savedCart,
-  },
-});
-
-// Save cart to localStorage whenever it changes
-store.subscribe(() => {
-  localStorage.setItem('cart', JSON.stringify(store.getState().cart));
 });
 
 export default store;
